c335075600de96d460976fb16804858a2af12511
gabrielcabral <cabraldealmeida@gmail.com>
2015-09-13 21:05:12 -0300

    correção

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -14,92 +14,95 @@ return array(
             'home' => array(
                 'type' => 'literal',
                 'options' => array(
-                    'route'    => '/',
+                    'route' => '/',
                     'defaults' => array(
                         'controller' => 'market-index-controller',
-                        'action'     => 'index',
+                        'action' => 'index',
                     )
                 )
             ),
             'market' => array(
                 'type' => 'literal',
                 'options' => array(
-                    'route'    => '/market',
+                    'route' => '/market',
                     'defaults' => array(
                         'controller' => 'market-index-controller',
-                        'action'     => 'index',
-                    )
-                )
-            ),
-            'market-post' => array(
-                'type' => 'literal',
-                'options' => array(
-                    'route'    => '/market/post',
-                    'defaults' => array(
-                        'controller' => 'market-post-controller',
-                        'action'     => 'index',
-                    )
-                ),
-            ),
-            'market-view' => array(
-                'type' => 'literal',
-                'options' => array(
-                    'route'    => '/market/view',
-                    'defaults' => array(
-                        'controller' => 'market-view-controller',
-                        'action'     => 'index',
+                        'action' => 'index',
                     )
                 ),
 
                 'may_terminate' => true,
                 'child_routes' => array(
-                    'index' => array(
-                        'type'    => 'Segment',
+
+                    'view' => array(
+                        'type' => 'literal',
                         'options' => array(
-                            'route'    => '/main[/:category]',
+                            'route' => '/view',
                             'defaults' => array(
-                                'action'     => 'index'
-                            ),
-                        ),
-                    ),
-                    'item' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/item[/:itemId]',
-                            'defaults' => array(
-                                'action'     => 'item'
-                            ),
-                            'constraints'=> array(
-                                'itemId'=>'[0-9]*'
+                                'controller' => 'market-view-controller',
+                                'action' => 'index',
                             )
                         ),
+
+                        'may_terminate' => true,
+                        'child_routes' => array(
+                            'main' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/main[/:category]',
+                                    'defaults' => array(
+                                        'action' => 'index'
+                                    )
+                                )
+                            ),
+                            'item' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/item[/:itemId]',
+                                    'defaults' => array(
+                                        'action' => 'item'
+                                    ),
+                                    'constraints' => array(
+                                        'itemId' => '[0-9]*'
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    'post' => array(
+                        'type' => 'literal',
+                        'options' => array(
+                            'route' => '/post',
+                            'defaults' => array(
+                                'controller' => 'market-post-controller',
+                                'action' => 'index',
+                            )
+                        )
                     ),
                 ),
             ),
         ),
     ),
 
-
-
-    'controllers' => array(
-        'invokables' => array(
-            'market-index-controller' => 'Market\Controller\IndexController',
-            'market-view-controller' => 'Market\Controller\ViewController'
+        'controllers' => array(
+            'invokables' => array(
+                'market-index-controller' => 'Market\Controller\IndexController',
+                'market-view-controller' => 'Market\Controller\ViewController'
+            ),
+            'factories' => array(
+                'market-post-controller' => 'Market\Factory\PostControllerFactory'
+            ),
+            'aliases' => array(
+                'alt' => 'market-view-controller'
+            )
         ),
-        'factories'=>array(
-            'market-post-controller' => 'Market\Factory\PostControllerFactory'
+
+
+        'view_manager' => array(
+
+            'template_path_stack' => array(
+                __DIR__ . '/../view',
+            ),
         ),
-        'aliases'=>array(
-            'alt'=>'market-view-controller'
-        )
-    ),
 
-
-    'view_manager' => array(
-
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-
-);
+    );
